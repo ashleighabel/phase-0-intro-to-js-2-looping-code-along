bcf5880e4670fb8bd89356b8876355064fbe1bf8
Ashleigh Abel <ashleighburgess1121@gmail.com>
2022-04-22 18:22:45 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const card = [];
+
+function writeCards(person, description) {
+  for (let i = 0; i < person.length; i++) {
+    card.push(`Thank you, ${person[i]}, for the wonderful ${description} gift!`);
+   }
+  return card;
+}
+
+function countDown(num) {
+    let i=10;
+    while (i >= 0) {
+        console.log (i);
+        i--
+    }
+    return num;
+}
